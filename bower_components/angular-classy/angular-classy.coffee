bdfcc511bf386a25c93fc118ec908d1413043d28
Dave Jeffery <dave@davejeffery.com>
2014-08-02 13:58:13 +0100

    Add dependencies for examples

---
+++ bower_components/angular-classy/angular-classy.coffee
@@ -0,0 +1,297 @@
+###
+Angular Classy 0.4.1
+Dave Jeffery, @DaveJ
+License: MIT
+###
+
+### global angular ###
+
+'use strict';
+
+defaults =
+  controller: {}
+
+selectorControllerCount = 0
+availablePlugins = {}
+enabledPlugins = {}
+
+enablePlugins = (reqs) ->
+  for req in reqs
+    for pluginName, plugin of availablePlugins
+      if pluginName is req
+        enabledPlugins[pluginName] = plugin
+        if plugin.options
+          defaults.controller[plugin.name] = angular.copy(plugin.options)
+
+pluginDo = (methodName, params, obj) ->
+  for pluginName, plugin of enabledPlugins
+    obj?.before?(plugin)
+    returnVal = plugin[methodName]?.apply(plugin, params)
+    obj?.after?(plugin, returnVal)
+
+copyAndExtendDeep = (dst) ->
+  for obj in arguments
+    if obj isnt dst
+      for key, value of obj
+        if dst[key] and dst[key].constructor and dst[key].constructor is Object
+          copyAndExtendDeep dst[key], value
+        else
+          dst[key] = angular.copy(value)
+  dst
+
+origModuleMethod = angular.module
+angular.module = (name, reqs, configFn) ->
+  ###
+  # We have to monkey-patch the `angular.module` method to see if 'classy' has been specified
+  # as a requirement. We also need the module name to we can register our classy controllers.
+  # Unfortunately there doesn't seem to be a more pretty/pluggable way to this.
+  ###
+  module = origModuleMethod(name, reqs, configFn)
+
+  # This is super messy.
+  # TODO: Clean this up and test the logic flow properly before moving to master
+  if reqs
+    enablePlugins(reqs)
+
+    if 'classy-core' in reqs or 'classy' in reqs
+      module.classy =
+        plugin:
+          controller: (plugin) -> availablePlugins[name] = plugin
+        options:
+          controller: {}
+
+        controller: (classObj) ->
+
+          class classyController
+            # `classyController` contains only a set of proxy functions for `classFns`,
+            # this is because I suspect that performance is better this way.
+            # TODO: Test performance to see if this is the most performant way to do it.
+
+            # Pre-initialisation (before instance is created)
+            classFns.preInit(@, classObj, module)
+
+            # Initialisation (after instance is created)
+            constructor: -> classFns.init(@, arguments, module)
+
+      module.cC = module.classy.controller
+
+  return module
+
+
+classFns =
+  localInject: ['$q']
+
+  preInit: (classConstructor, classObj, module) ->
+    for own key, value of classObj
+      classConstructor::[key] = value
+
+    options =
+      copyAndExtendDeep {}, defaults.controller, module.classy.options.controller, classObj.__options
+
+
+    pluginDo 'preInitBefore', [classConstructor, classObj, module],
+      before: (plugin) ->
+        plugin.options = options[plugin.name] or {}
+    pluginDo 'preInit', [classConstructor, classObj, module]
+    pluginDo 'preInitAfter', [classConstructor, classObj, module]
+
+  init: (klass, $inject, module) ->
+    injectIndex = 0
+    deps = {}
+    for key in klass.constructor.__classDepNames
+      deps[key] = $inject[injectIndex]
+      injectIndex++
+
+    pluginDo 'null', [],
+      before: (plugin) ->
+        if angular.isArray(plugin.localInject)
+          for depName in plugin.localInject
+            dep = $inject[injectIndex]
+            plugin[depName] = dep
+            injectIndex++
+
+    for depName in @localInject
+      dep = $inject[injectIndex]
+      @[depName] = dep
+      injectIndex++
+
+    pluginDo 'initBefore', [klass, deps, module]
+
+    pluginPromises = []
+    pluginDo 'init', [klass, deps, module],
+      after: (plugin, returnVal) ->
+        if returnVal && returnVal.then
+          # Naively assume this is a promise
+          # TODO: Make this smarter than just looking for `.then`
+          pluginPromises.push(returnVal)
+
+    initClass = ->
+      klass.init?()
+      pluginDo 'initAfter', [klass, deps, module]
+      @postInit(klass, deps, module)
+
+    if pluginPromises.length
+      @$q.all(pluginPromises).then angular.bind(@, initClass)
+    else
+      angular.bind(@, initClass)()
+
+  postInit: (klass, deps, module) ->
+    pluginDo 'postInitBefore', [klass, deps, module]
+    pluginDo 'postInit', [klass, deps, module]
+    pluginDo 'postInitAfter', [klass, deps, module]
+
+angular.module('classy-core', [])
+angular.module('classy-addFnsToScope', ['classy-core']).classy.plugin.controller
+  name: 'addFnsToScope'
+
+  options:
+    enabled: true
+    privateMethodPrefix: '_'
+    ignore: ['constructor', 'init']
+
+  hasPrivateMethodPrefix: (string) ->
+    prefix = @options.privateMethodPrefix
+    if !prefix then false
+    else string.slice(0, prefix.length) is prefix
+
+  init: (klass, deps, module) ->
+    # Adds controller functions (unless they have a `_` prefix) to the `$scope`
+    if @options.enabled
+      for key, fn of klass.constructor::
+        if angular.isFunction(fn) and !(key in @options.ignore)
+          klass[key] = angular.bind(klass, fn)
+          if !@hasPrivateMethodPrefix(key) and deps.$scope
+            deps.$scope[key] = klass[key]
+angular.module('classy-bindDependencies', ['classy-core']).classy.plugin.controller
+  name: 'bindDependencies'
+
+  options:
+    enabled: true
+    scopeShortcut: '$'
+    useExistingNameString: '.'
+
+  preInit: (classConstructor, classObj, module) ->
+    depNames = classObj.inject or []
+
+    # Inject the `deps` if it's passed in as an array
+    if angular.isArray(depNames) then @inject(classConstructor, depNames)
+
+    # If `deps` is object: Wrap object in array and then inject
+    else if angular.isObject(depNames) then @inject(classConstructor, [depNames], classObj)
+
+  inject: (classConstructor, depNames, classObj) ->
+    if angular.isObject depNames[0]
+      classConstructor.__classyControllerInjectObject = depNames[0]
+      depNames = (service for service, name of depNames[0])
+
+    pluginDepNames = []
+    for pluginName, plugin of enabledPlugins
+      if angular.isArray(plugin.localInject)
+        pluginDepNames = pluginDepNames.concat(plugin.localInject).concat(classFns.localInject)
+
+    classConstructor.__classDepNames = angular.copy depNames
+
+    # Add the `deps` to the controller's $inject annotations.
+    classConstructor.$inject = depNames.concat pluginDepNames
+
+  init: (klass, deps, module) ->
+    if @options.enabled
+      injectObject = klass.constructor.__classyControllerInjectObject
+
+      # Takes the `$inject` dependencies and assigns a class-wide (`@`) variable to each one.
+      for key, i in klass.constructor.$inject
+        dependency = deps[key]
+
+        # If the named dependency is assigned a name that is different from the original name
+        if (injectObject and (injectName = injectObject[key]) and
+            injectName != @options.useExistingNameString)
+          klass[injectName] = dependency
+        else
+          klass[key] = dependency
+
+          if key is '$scope' and @options.scopeShortcut
+            # Add a shortcut to the $scope (by default `@$`)
+            klass[@options.scopeShortcut] = klass[key]
+angular.module('classy-register', ['classy-core']).classy.plugin.controller
+  name: 'registerSelector'
+
+  preInit: (classConstructor, classObj, module) ->
+    if angular.isString(classObj.name)
+      # Register the controller using name
+      module.controller classObj.name, classConstructor
+angular.module('classy-registerSelector', ['classy-core']).classy.plugin.controller
+  name: 'register'
+
+  preInit: (classConstructor, classObj, module) ->
+    if classObj.el || classObj.selector
+      # Register the controller using selector
+      @registerSelector(module, classObj.el || classObj.selector, classConstructor)
+
+  registerSelector: (module, selector, classConstructor) ->
+    selectorControllerCount++
+    controllerName = "ClassySelector#{selectorControllerCount}Controller"
+    module.controller controllerName, classConstructor
+
+    if angular.isElement(selector)
+      selector.setAttribute('data-ng-controller', controllerName)
+      return
+
+    if angular.isString(selector)
+      # Query the dom using jQuery if available, otherwise fallback to qSA
+      els = window.jQuery?(selector) or document.querySelectorAll(selector)
+    else if angular.isArray(selector)
+      els = selector
+    else return
+
+    for el in els
+      if angular.isElement(el)
+        el.setAttribute('data-ng-controller', controllerName)
+angular.module('classy-watch', ['classy-core']).classy.plugin.controller
+  name: 'watch'
+
+  options:
+    enabled: true
+    _watchKeywords:
+      normal: []
+      objectEquality: ['{object}', '{deep}']
+      collection: ['{collection}', '{shallow}']
+
+  isActive: (klass, deps) ->
+    if @options.enabled and angular.isObject(klass.watch)
+      if !deps.$scope
+        throw new Error "You need to inject `$scope` to use the watch object"
+        return false
+
+      return true
+
+  watchFns:
+    normal: (klass, expression, fn, deps) ->
+        deps.$scope.$watch(expression, angular.bind(klass, fn))
+    objectEquality: (klass, expression, fn, deps) ->
+        deps.$scope.$watch(expression, angular.bind(klass, fn), true)
+    collection: (klass, expression, fn, deps) ->
+        deps.$scope.$watchCollection(expression, angular.bind(klass, fn))
+
+  postInit: (klass, deps, module) ->
+    if !@isActive(klass, deps) then return
+
+    watchKeywords = @options._watchKeywords
+
+    for expression, fn of klass.watch
+      if angular.isString(fn) then fn = klass[fn]
+      if angular.isString(expression) and angular.isFunction(fn)
+        watchRegistered = false
+
+        # Search for keywords that identify it is a non-standard watch
+        for watchType, watchFn of @watchFns
+          if watchRegistered then break
+          for keyword in watchKeywords[watchType]
+            if expression.indexOf(keyword) isnt -1
+              watchFn(klass, expression.replace(keyword, ''), fn, deps)
+              watchRegistered = true
+              break
+
+        # If no keywords have been found then register it as a normal watch
+        if !watchRegistered then this.watchFns.normal(klass, expression, fn, deps)
+
+angular.module 'classy', ["classy-addFnsToScope","classy-bindDependencies","classy-register","classy-registerSelector","classy-watch"]
---
+++ bower_components/angular-classy/angular-classy.js
@@ -0,0 +1,439 @@
+
+/*
+Angular Classy 0.4.1
+Dave Jeffery, @DaveJ
+License: MIT
+ */
+
+
+/* global angular */
+
+(function() {
+  'use strict';
+  var availablePlugins, classFns, copyAndExtendDeep, defaults, enablePlugins, enabledPlugins, origModuleMethod, pluginDo, selectorControllerCount,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
+    __hasProp = {}.hasOwnProperty;
+
+  defaults = {
+    controller: {}
+  };
+
+  selectorControllerCount = 0;
+
+  availablePlugins = {};
+
+  enabledPlugins = {};
+
+  enablePlugins = function(reqs) {
+    var plugin, pluginName, req, _i, _len, _results;
+    _results = [];
+    for (_i = 0, _len = reqs.length; _i < _len; _i++) {
+      req = reqs[_i];
+      _results.push((function() {
+        var _results1;
+        _results1 = [];
+        for (pluginName in availablePlugins) {
+          plugin = availablePlugins[pluginName];
+          if (pluginName === req) {
+            enabledPlugins[pluginName] = plugin;
+            if (plugin.options) {
+              _results1.push(defaults.controller[plugin.name] = angular.copy(plugin.options));
+            } else {
+              _results1.push(void 0);
+            }
+          } else {
+            _results1.push(void 0);
+          }
+        }
+        return _results1;
+      })());
+    }
+    return _results;
+  };
+
+  pluginDo = function(methodName, params, obj) {
+    var plugin, pluginName, returnVal, _ref, _results;
+    _results = [];
+    for (pluginName in enabledPlugins) {
+      plugin = enabledPlugins[pluginName];
+      if (obj != null) {
+        if (typeof obj.before === "function") {
+          obj.before(plugin);
+        }
+      }
+      returnVal = (_ref = plugin[methodName]) != null ? _ref.apply(plugin, params) : void 0;
+      _results.push(obj != null ? typeof obj.after === "function" ? obj.after(plugin, returnVal) : void 0 : void 0);
+    }
+    return _results;
+  };
+
+  copyAndExtendDeep = function(dst) {
+    var key, obj, value, _i, _len;
+    for (_i = 0, _len = arguments.length; _i < _len; _i++) {
+      obj = arguments[_i];
+      if (obj !== dst) {
+        for (key in obj) {
+          value = obj[key];
+          if (dst[key] && dst[key].constructor && dst[key].constructor === Object) {
+            copyAndExtendDeep(dst[key], value);
+          } else {
+            dst[key] = angular.copy(value);
+          }
+        }
+      }
+    }
+    return dst;
+  };
+
+  origModuleMethod = angular.module;
+
+  angular.module = function(name, reqs, configFn) {
+
+    /*
+     * We have to monkey-patch the `angular.module` method to see if 'classy' has been specified
+     * as a requirement. We also need the module name to we can register our classy controllers.
+     * Unfortunately there doesn't seem to be a more pretty/pluggable way to this.
+     */
+    var module;
+    module = origModuleMethod(name, reqs, configFn);
+    if (reqs) {
+      enablePlugins(reqs);
+      if (__indexOf.call(reqs, 'classy-core') >= 0 || __indexOf.call(reqs, 'classy') >= 0) {
+        module.classy = {
+          plugin: {
+            controller: function(plugin) {
+              return availablePlugins[name] = plugin;
+            }
+          },
+          options: {
+            controller: {}
+          },
+          controller: function(classObj) {
+            var classyController;
+            return classyController = (function() {
+              classFns.preInit(classyController, classObj, module);
+
+              function classyController() {
+                classFns.init(this, arguments, module);
+              }
+
+              return classyController;
+
+            })();
+          }
+        };
+        module.cC = module.classy.controller;
+      }
+    }
+    return module;
+  };
+
+  classFns = {
+    localInject: ['$q'],
+    preInit: function(classConstructor, classObj, module) {
+      var key, options, value;
+      for (key in classObj) {
+        if (!__hasProp.call(classObj, key)) continue;
+        value = classObj[key];
+        classConstructor.prototype[key] = value;
+      }
+      options = copyAndExtendDeep({}, defaults.controller, module.classy.options.controller, classObj.__options);
+      pluginDo('preInitBefore', [classConstructor, classObj, module], {
+        before: function(plugin) {
+          return plugin.options = options[plugin.name] || {};
+        }
+      });
+      pluginDo('preInit', [classConstructor, classObj, module]);
+      return pluginDo('preInitAfter', [classConstructor, classObj, module]);
+    },
+    init: function(klass, $inject, module) {
+      var dep, depName, deps, initClass, injectIndex, key, pluginPromises, _i, _j, _len, _len1, _ref, _ref1;
+      injectIndex = 0;
+      deps = {};
+      _ref = klass.constructor.__classDepNames;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        key = _ref[_i];
+        deps[key] = $inject[injectIndex];
+        injectIndex++;
+      }
+      pluginDo('null', [], {
+        before: function(plugin) {
+          var dep, depName, _j, _len1, _ref1, _results;
+          if (angular.isArray(plugin.localInject)) {
+            _ref1 = plugin.localInject;
+            _results = [];
+            for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+              depName = _ref1[_j];
+              dep = $inject[injectIndex];
+              plugin[depName] = dep;
+              _results.push(injectIndex++);
+            }
+            return _results;
+          }
+        }
+      });
+      _ref1 = this.localInject;
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        depName = _ref1[_j];
+        dep = $inject[injectIndex];
+        this[depName] = dep;
+        injectIndex++;
+      }
+      pluginDo('initBefore', [klass, deps, module]);
+      pluginPromises = [];
+      pluginDo('init', [klass, deps, module], {
+        after: function(plugin, returnVal) {
+          if (returnVal && returnVal.then) {
+            return pluginPromises.push(returnVal);
+          }
+        }
+      });
+      initClass = function() {
+        if (typeof klass.init === "function") {
+          klass.init();
+        }
+        pluginDo('initAfter', [klass, deps, module]);
+        return this.postInit(klass, deps, module);
+      };
+      if (pluginPromises.length) {
+        return this.$q.all(pluginPromises).then(angular.bind(this, initClass));
+      } else {
+        return angular.bind(this, initClass)();
+      }
+    },
+    postInit: function(klass, deps, module) {
+      pluginDo('postInitBefore', [klass, deps, module]);
+      pluginDo('postInit', [klass, deps, module]);
+      return pluginDo('postInitAfter', [klass, deps, module]);
+    }
+  };
+
+  angular.module('classy-core', []);
+
+  angular.module('classy-addFnsToScope', ['classy-core']).classy.plugin.controller({
+    name: 'addFnsToScope',
+    options: {
+      enabled: true,
+      privateMethodPrefix: '_',
+      ignore: ['constructor', 'init']
+    },
+    hasPrivateMethodPrefix: function(string) {
+      var prefix;
+      prefix = this.options.privateMethodPrefix;
+      if (!prefix) {
+        return false;
+      } else {
+        return string.slice(0, prefix.length) === prefix;
+      }
+    },
+    init: function(klass, deps, module) {
+      var fn, key, _ref, _results;
+      if (this.options.enabled) {
+        _ref = klass.constructor.prototype;
+        _results = [];
+        for (key in _ref) {
+          fn = _ref[key];
+          if (angular.isFunction(fn) && !(__indexOf.call(this.options.ignore, key) >= 0)) {
+            klass[key] = angular.bind(klass, fn);
+            if (!this.hasPrivateMethodPrefix(key) && deps.$scope) {
+              _results.push(deps.$scope[key] = klass[key]);
+            } else {
+              _results.push(void 0);
+            }
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      }
+    }
+  });
+
+  angular.module('classy-bindDependencies', ['classy-core']).classy.plugin.controller({
+    name: 'bindDependencies',
+    options: {
+      enabled: true,
+      scopeShortcut: '$',
+      useExistingNameString: '.'
+    },
+    preInit: function(classConstructor, classObj, module) {
+      var depNames;
+      depNames = classObj.inject || [];
+      if (angular.isArray(depNames)) {
+        return this.inject(classConstructor, depNames);
+      } else if (angular.isObject(depNames)) {
+        return this.inject(classConstructor, [depNames], classObj);
+      }
+    },
+    inject: function(classConstructor, depNames, classObj) {
+      var name, plugin, pluginDepNames, pluginName, service;
+      if (angular.isObject(depNames[0])) {
+        classConstructor.__classyControllerInjectObject = depNames[0];
+        depNames = (function() {
+          var _ref, _results;
+          _ref = depNames[0];
+          _results = [];
+          for (service in _ref) {
+            name = _ref[service];
+            _results.push(service);
+          }
+          return _results;
+        })();
+      }
+      pluginDepNames = [];
+      for (pluginName in enabledPlugins) {
+        plugin = enabledPlugins[pluginName];
+        if (angular.isArray(plugin.localInject)) {
+          pluginDepNames = pluginDepNames.concat(plugin.localInject).concat(classFns.localInject);
+        }
+      }
+      classConstructor.__classDepNames = angular.copy(depNames);
+      return classConstructor.$inject = depNames.concat(pluginDepNames);
+    },
+    init: function(klass, deps, module) {
+      var dependency, i, injectName, injectObject, key, _i, _len, _ref, _results;
+      if (this.options.enabled) {
+        injectObject = klass.constructor.__classyControllerInjectObject;
+        _ref = klass.constructor.$inject;
+        _results = [];
+        for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+          key = _ref[i];
+          dependency = deps[key];
+          if (injectObject && (injectName = injectObject[key]) && injectName !== this.options.useExistingNameString) {
+            _results.push(klass[injectName] = dependency);
+          } else {
+            klass[key] = dependency;
+            if (key === '$scope' && this.options.scopeShortcut) {
+              _results.push(klass[this.options.scopeShortcut] = klass[key]);
+            } else {
+              _results.push(void 0);
+            }
+          }
+        }
+        return _results;
+      }
+    }
+  });
+
+  angular.module('classy-register', ['classy-core']).classy.plugin.controller({
+    name: 'registerSelector',
+    preInit: function(classConstructor, classObj, module) {
+      if (angular.isString(classObj.name)) {
+        return module.controller(classObj.name, classConstructor);
+      }
+    }
+  });
+
+  angular.module('classy-registerSelector', ['classy-core']).classy.plugin.controller({
+    name: 'register',
+    preInit: function(classConstructor, classObj, module) {
+      if (classObj.el || classObj.selector) {
+        return this.registerSelector(module, classObj.el || classObj.selector, classConstructor);
+      }
+    },
+    registerSelector: function(module, selector, classConstructor) {
+      var controllerName, el, els, _i, _len, _results;
+      selectorControllerCount++;
+      controllerName = "ClassySelector" + selectorControllerCount + "Controller";
+      module.controller(controllerName, classConstructor);
+      if (angular.isElement(selector)) {
+        selector.setAttribute('data-ng-controller', controllerName);
+        return;
+      }
+      if (angular.isString(selector)) {
+        els = (typeof window.jQuery === "function" ? window.jQuery(selector) : void 0) || document.querySelectorAll(selector);
+      } else if (angular.isArray(selector)) {
+        els = selector;
+      } else {
+        return;
+      }
+      _results = [];
+      for (_i = 0, _len = els.length; _i < _len; _i++) {
+        el = els[_i];
+        if (angular.isElement(el)) {
+          _results.push(el.setAttribute('data-ng-controller', controllerName));
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    }
+  });
+
+  angular.module('classy-watch', ['classy-core']).classy.plugin.controller({
+    name: 'watch',
+    options: {
+      enabled: true,
+      _watchKeywords: {
+        normal: [],
+        objectEquality: ['{object}', '{deep}'],
+        collection: ['{collection}', '{shallow}']
+      }
+    },
+    isActive: function(klass, deps) {
+      if (this.options.enabled && angular.isObject(klass.watch)) {
+        if (!deps.$scope) {
+          throw new Error("You need to inject `$scope` to use the watch object");
+          return false;
+        }
+        return true;
+      }
+    },
+    watchFns: {
+      normal: function(klass, expression, fn, deps) {
+        return deps.$scope.$watch(expression, angular.bind(klass, fn));
+      },
+      objectEquality: function(klass, expression, fn, deps) {
+        return deps.$scope.$watch(expression, angular.bind(klass, fn), true);
+      },
+      collection: function(klass, expression, fn, deps) {
+        return deps.$scope.$watchCollection(expression, angular.bind(klass, fn));
+      }
+    },
+    postInit: function(klass, deps, module) {
+      var expression, fn, keyword, watchFn, watchKeywords, watchRegistered, watchType, _i, _len, _ref, _ref1, _ref2, _results;
+      if (!this.isActive(klass, deps)) {
+        return;
+      }
+      watchKeywords = this.options._watchKeywords;
+      _ref = klass.watch;
+      _results = [];
+      for (expression in _ref) {
+        fn = _ref[expression];
+        if (angular.isString(fn)) {
+          fn = klass[fn];
+        }
+        if (angular.isString(expression) && angular.isFunction(fn)) {
+          watchRegistered = false;
+          _ref1 = this.watchFns;
+          for (watchType in _ref1) {
+            watchFn = _ref1[watchType];
+            if (watchRegistered) {
+              break;
+            }
+            _ref2 = watchKeywords[watchType];
+            for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+              keyword = _ref2[_i];
+              if (expression.indexOf(keyword) !== -1) {
+                watchFn(klass, expression.replace(keyword, ''), fn, deps);
+                watchRegistered = true;
+                break;
+              }
+            }
+          }
+          if (!watchRegistered) {
+            _results.push(this.watchFns.normal(klass, expression, fn, deps));
+          } else {
+            _results.push(void 0);
+          }
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    }
+  });
+
+  angular.module('classy', ["classy-addFnsToScope", "classy-bindDependencies", "classy-register", "classy-registerSelector", "classy-watch"]);
+
+}).call(this);
