9c0cca933bc4a245ed2b148bc5902c08f935275e
Dave Jeffery <dave@davejeffery.com>
2014-08-02 13:57:02 +0100

    Better docs, change code formatting to spaces

--- README.md
+++ README.md
@@ -1,3 +1,113 @@
-Tests and more info coming soon.
+#### Computed Properties plugin for Angular Classy
 
-For now, run `bower install` to install dependencies and then have a look at the files in the examples folder.
+This is plugin that makes it possible to do computed set & get properties with Angular Classy.
+
+## Install
+
+Install with `bower`:
+
+```shell
+bower install classy-computed
+```
+
+Add to your `index.html`:
+
+```html
+<script src="/bower_components/angular/angular.js"></script>
+<script src="/bower_components/angular-classy/angular-classy.js"></script>
+<script src="/bower_components/classy-computed/classy-computed.js"></script>
+```
+
+Add `classy` and `classy-computed` to your application module.
+
+```javascript
+var app = angular.module('app', ['classy', 'classy-computed']);
+```
+
+## Usage Examples
+
+### Simple computed property
+
+```javascript
+app.classy.controller({
+	
+	// ...
+
+	computed: {
+		fullName: function() {
+			return this.$.firstName + ' ' + this.$.secondName;
+		}
+	}
+});
+```
+
+### Computed property with watch field for better performance
+
+Adding a watch field will give you better performance because it will only execute the computed expression when one of the watched properties has changed. In our trivial example below it will give you very little performance benefit, but for more complicated expressions it can give a significant speedup.
+
+```javascript
+app.classy.controller({
+	
+	// ...
+
+	computed: {
+		fullName: {
+			watch: ['firstName', 'secondName'],
+			get: function() {
+					return this.$.firstName + ' ' + this.$.secondName;
+			}
+		}
+	}
+});
+```
+
+### Computed setters
+
+You can also do computed setters and even combine with computed getters.
+
+```javascript
+app.classy.controller({
+	
+	// ...
+
+	computed: {
+		fullName: {
+			get: function() {
+					return this.$.firstName + ' ' + this.$.secondName;
+			},
+			set: function(fullName) {
+				if (fullName && fullName.split(' ')) {
+					var names = fullName.split(' ');
+					this.$.firstName = names[0] || '';
+					if (names.length > 1) {
+						this.$.secondName = names[names.length - 1] || '';
+					}
+				}
+			}
+		}
+	}
+});
+```
+
+
+## License
+
+The MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- examples/computed.js
+++ examples/computed.js
@@ -5,117 +5,117 @@
 var app = angular.module('app', ['classy', 'classy-computed']);
 
 app.classy.controller({
-	name: 'ComputedController',
-	inject: ['$scope'],
+  name: 'ComputedController',
+  inject: ['$scope'],
 
-	init: function() {
-		this.$.firstName = 'Carlton';
-		this.$.secondName = 'Bank';
+  init: function() {
+    this.$.firstName = 'Karlton';
+    this.$.secondName = 'Banks';
 
-		// Easter Egg :-)
-		this.$.dancingCarltonCSSProp = 'url(http://media.giphy.com/media/uK1m1ZAJAOr8k/giphy.gif) no-repeat top right'
-	},
+    // Easter Egg :-)
+    this.$.dancingCarltonCSSProp = 'url(http://media.giphy.com/media/uK1m1ZAJAOr8k/giphy.gif) no-repeat top right'
+  },
 
-	computed: {
-		fullName: function() {
-			return this.$.firstName + ' ' + this.$.secondName;
-		}
-	}
+  computed: {
+    fullName: function() {
+      return this.$.firstName + ' ' + this.$.secondName;
+    }
+  }
 });
 
 app.classy.controller({
-	name: 'ComputedWithWatchController',
-	inject: ['$scope'],
+  name: 'ComputedWithWatchController',
+  inject: ['$scope'],
 
-	init: function() {
-		this.$.firstName = 'Geoffrey';
-		this.$.secondName = 'Butler';
-	},
+  init: function() {
+    this.$.firstName = 'Geoffrey';
+    this.$.secondName = 'Butler';
+  },
 
-	computed: {
-		fullName: {
-			watch: ['firstName', 'secondName'],
-			get: function() {
-					return this.$.firstName + ' ' + this.$.secondName;
-			}
-		}
-	}
+  computed: {
+    fullName: {
+      watch: ['firstName', 'secondName'],
+      get: function() {
+          return this.$.firstName + ' ' + this.$.secondName;
+      }
+    }
+  }
 });
 
 app.classy.controller({
-	name: 'ComputedSetController',
-	inject: ['$scope'],
+  name: 'ComputedSetController',
+  inject: ['$scope'],
 
-	init: function() {
-		this.$.fullName = 'Will Smith';
-	},
+  init: function() {
+    this.$.fullName = 'Will Smith';
+  },
 
-	computed: {
-		fullName: {
-			set: function(fullName) {
-				if (fullName && fullName.split(' ')) {
-					var names = fullName.split(' ');
-					this.$.firstName = names[0] || '';
-					if (names.length > 1) {
-						this.$.secondName = names[names.length - 1] || '';
-					}
-				}
-			}
-		}
-	}
+  computed: {
+    fullName: {
+      set: function(fullName) {
+        if (fullName && fullName.split(' ')) {
+          var names = fullName.split(' ');
+          this.$.firstName = names[0] || '';
+          if (names.length > 1) {
+            this.$.secondName = names[names.length - 1] || '';
+          }
+        }
+      }
+    }
+  }
 });
 
 app.classy.controller({
-	name: 'ComputedGetSetController',
-	inject: ['$scope'],
+  name: 'ComputedGetSetController',
+  inject: ['$scope'],
 
-	init: function() {
-		this.$.firstName = 'Tyriq';
-		this.$.secondName = 'Johnson';
-	},
+  init: function() {
+    this.$.firstName = 'Tyriq';
+    this.$.secondName = 'Johnson';
+  },
 
-	computed: {
-		fullName: {
-			get: function() {
-					return this.$.firstName + ' ' + this.$.secondName;
-			},
-			set: function(fullName) {
-				if (fullName && fullName.split(' ')) {
-					var names = fullName.split(' ');
-					this.$.firstName = names[0] || '';
-					if (names.length > 1) {
-						this.$.secondName = names[names.length - 1] || '';
-					}
-				}
-			}
-		}
-	}
+  computed: {
+    fullName: {
+      get: function() {
+          return this.$.firstName + ' ' + this.$.secondName;
+      },
+      set: function(fullName) {
+        if (fullName && fullName.split(' ')) {
+          var names = fullName.split(' ');
+          this.$.firstName = names[0] || '';
+          if (names.length > 1) {
+            this.$.secondName = names[names.length - 1] || '';
+          }
+        }
+      }
+    }
+  }
 });
 
 app.classy.controller({
-	name: 'ComputedGetSetWithWatchController',
-	inject: ['$scope'],
+  name: 'ComputedGetSetWithWatchController',
+  inject: ['$scope'],
 
-	init: function() {
-		this.$.firstName = 'Jazzy';
-		this.$.secondName = 'Jeff';
-	},
+  init: function() {
+    this.$.firstName = 'Jazzy';
+    this.$.secondName = 'Jeff';
+  },
 
-	computed: {
-		fullName: {
-			watch: ['firstName', 'secondName'],
-			get: function() {
-					return this.$.firstName + ' ' + this.$.secondName;
-			},
-			set: function(fullName) {
-				if (fullName && fullName.split(' ')) {
-					var names = fullName.split(' ');
-					this.$.firstName = names[0] || '';
-					if (names.length > 1) {
-						this.$.secondName = names[names.length - 1] || '';
-					}
-				}
-			}
-		}
-	}
+  computed: {
+    fullName: {
+      watch: ['firstName', 'secondName'],
+      get: function() {
+          return this.$.firstName + ' ' + this.$.secondName;
+      },
+      set: function(fullName) {
+        if (fullName && fullName.split(' ')) {
+          var names = fullName.split(' ');
+          this.$.firstName = names[0] || '';
+          if (names.length > 1) {
+            this.$.secondName = names[names.length - 1] || '';
+          }
+        }
+      }
+    }
+  }
 });
